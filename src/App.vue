6eca166309d9b397e99ef3a66999fadadb5d2ace
schlucht <web@jagolo.ch>
2022-01-11 05:48:50 +0100

    Initial Project

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,9 @@ export default {
 </script>
 
 <style>
+@import "~bootstrap/dist/css/bootstrap.min.css";
+@import "~@fortawesome/fontawesome-free/css/all.min.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
